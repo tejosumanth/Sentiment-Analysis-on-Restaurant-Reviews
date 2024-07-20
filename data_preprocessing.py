6efb2d6883d245f7bcb855b18a0d60735cc1ed2e
tejosumanth <pavuluritejo@gmail.com>
2024-07-20 20:21:00 +0000

    Complete data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -214,3 +214,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Define the model
 model = Sequential()
 model.add(GRU(100, input_shape=(max_sequence_length, 100), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(GRU(50))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, batch_size=64)
