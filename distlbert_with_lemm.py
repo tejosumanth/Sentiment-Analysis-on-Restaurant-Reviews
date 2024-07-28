fb2a21cb619b7f08b84425484c803fb090e7bc2d
tejosumanth <pavuluritejo@gmail.com>
2024-07-28 21:19:00 +0000

    Add more functions to distlbert_with_lemm.py

--- distlbert_with_lemm.py
+++ distlbert_with_lemm.py
@@ -223,3 +223,12 @@ fpr, tpr, thresholds = roc_curve(y_true, y_pred)
 plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+import tensorflow as tf
+from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
